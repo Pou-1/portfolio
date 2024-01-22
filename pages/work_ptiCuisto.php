a420e0e2ed36a0750caff3f0e67f3dbe31a515fe
Cyprien <cypr.jeux@gmail.com>
2024-01-22 06:13:34 +0100

    Pticuisto Work

--- pages/work_ptiCuisto.php
+++ pages/work_ptiCuisto.php
@@ -9,10 +9,46 @@
     <body>
         <?php include('component/navbar.php'); ?>
 
-            <div class="main">
-                <h1 class="WorkPageTitle">Pti Cuisto</h1>
+        <div class="main aligne Column">
+            <h1 class="WorkPageTitle">Pti Cuisto</h1>
+
+            <p>PtiCuisto is a project i did with classmates, we got a client asking
+                for a website to teach to the children how to cook</p>
+                
+            <h2 class="h2Title">Constraints</h2>
+
+            <div class="Subtitle SubtitleWhite Column">
+                <h1>Visual identity</h1>
+                <hr>
             </div>
 
+            <div class="Line VisualIdentity">
+                <p class="aligne">#2a3990</p>
+                <p class="aligne">#ffffff</p>
+                <p class="aligne">#999999</p>
+                <p class="aligne">#7890cd</p>
+            </div>
+
+            <div class="Subtitle SubtitleWhite Column">
+                <h1>Others Constraints</h1>
+                <hr>
+            </div>
+            <ul>
+                <li>Use a MVC design pattern</li>
+                <li>Store recipe data in a MySQL database</li>
+                <li>Use the framework Bootrstrap</li>
+                <li>the website need to be responsive and compliant with W3C HTML and CSS validators</li>
+                <li>Meet the accessibility requirements of WCAG AA 2.0</li>
+            </ul>
+            <h2 class="h2Title">Final product</h2>
+            <div class="Column ColumnShare">
+                <img src="../../img/PtiCuisto/Accueil.png" alt="">
+                <img src="../../img/PtiCuisto/Liste_recettes.png" alt="">
+                <img src="../../img/PtiCuisto/Mousse_chocolat.png" alt="">
+                <img src="../../img/PtiCuisto/inscrire.png" alt="">
+                <img src="../../img/PtiCuisto/Mon_compte.png" alt="">
+            </div>
+        </div>
     </body>
     <script src="../javascript/script.js"></script>
 
